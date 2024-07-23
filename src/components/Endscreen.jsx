9ff81db7ae198ebad7dbfc551ce99bb0438cc144
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-07-23 11:17:58 +0700

    Updated endscreen message and web icon

--- src/components/Endscreen.jsx
+++ src/components/Endscreen.jsx
@@ -4,7 +4,11 @@ export default function Endscreen({ handleRestart, bestScoreboard }) {
   return (
     <div className="endScreenContainer">
       <h2>Game Over!</h2>
-      <p>Your best score is {bestScoreboard}!</p>
+      <p>
+        {bestScoreboard === 20
+          ? `Congratulations! You scored 20 out of 20, you have an amazing memory`
+          : `Good try, your best score is ${bestScoreboard} out of 20!`}
+      </p>
       <button className="restartBtn" onClick={() => handleRestart("game")}>
         Restart Game!
       </button>
